a88b0db0e40b102be11cb3f807f6d25cd6266094
mustafa <msolmaz83@gmail.com>
2023-12-28 00:08:01 +0100

    router

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
-
-import './App.css'
-
+import AutLayout from "./_auth/forms/AutLayout";
+import SigninForm from "./_auth/forms/SigninForm";
+import SignupForm from "./_auth/forms/SignupForm";
+import RootLayout from "./_root/RootLayout";
+import { Home } from "./_root/pages";
+import "./globals.css"
+import { Routes, Route } from "react-router-dom";
 function App() {
-
-
   return (
     <>
-      hhhhhhhhhhh
+      <main>
+        <Routes>
+          {/** Public Router */}
+          <Route element={<AutLayout />}>
+            <Route  path="/sign-in" element={<SigninForm />} />
+            <Route  path="/sign-up" element={<SignupForm />} />
+          </Route>
+
+          {/** Private Router */}
+          <Route element={<RootLayout/>}>
+            <Route index element={<Home />} />
+          </Route>
+          
+        </Routes>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/_auth/forms/AutLayout.tsx
@@ -0,0 +1,11 @@
+
+import { Outlet } from "react-router-dom"
+
+
+const AutLayout = () => {
+  return (
+    <div><Outlet/></div>
+  )
+}
+
+export default AutLayout
