ebe39395b5e416833211d406b03e0f7c1f7dea4e
mustafa <msolmaz83@gmail.com>
2023-12-28 00:23:48 +0100

    button

--- src/_auth/forms/AutLayout.tsx
+++ src/_auth/forms/AutLayout.tsx
@@ -1,11 +1,20 @@
-
-import { Outlet } from "react-router-dom"
-
+import { Navigate, Outlet } from "react-router-dom";
 
 const AutLayout = () => {
+  const isAuthenticated = false;
   return (
-    <div><Outlet/></div>
-  )
-}
+    <div>
+      {isAuthenticated ? (
+        <Navigate to="/" />
+      ) : (
+        <>
+          <section className="flex flex-1 justify-center items-center flex-col py-10">
+            <Outlet />
+          </section>
+        </>
+      )}
+    </div>
+  );
+};
 
-export default AutLayout
+export default AutLayout;
