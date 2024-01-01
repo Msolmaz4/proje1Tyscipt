742a49f1419f18bb0844aaf906544e74003ae9b7
mustafa <msolmaz83@gmail.com>
2024-01-01 22:38:24 +0100

    signup

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,9 +1,124 @@
 import { Button } from "@/components/ui/button"
 
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { SignupValidation } from "@/lib/validation"
+import { z } from "zod"
+import { Divide } from "lucide-react"
+import Loader from "@/components/shared/Loader"
+
+
 
 const SignupForm = () => {
+const isLoading = true
+
+
+ // 1. Define your form.
+ const form = useForm<z.infer<typeof SignupValidation>>({
+  resolver: zodResolver(SignupValidation),
+  defaultValues: {
+    name: "",
+    username: "",
+    email: "",
+    password: "",
+  },
+})
+
+// 2. Define a submit handler.
+function onSubmit(values: z.infer<typeof SignupValidation>) {
+  // Do something with the form values.
+  // ✅ This will be type-safe and validated.
+  console.log(values)
+}
+
   return (
-    <div> <Button>Click me</Button></div>
+      <Form {...form}>
+
+ <div className="sm:w-420 flex-center flex-col">
+  <img src="assets/images/logo.svg" alt="logo" />
+  <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">Create a new account</h2>
+  <p className="text-light-3 small-medium md:base-regular mt-2"> To use Snapgram enter your details</p>
+ 
+
+
+    <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5 w-full mt-4" > 
+      <FormField
+        control={form.control}
+        name="name"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Name</FormLabel>
+            <FormControl>
+              <Input placeholder="name" {...field} type="text" className="shad-input" />
+            </FormControl>
+            
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name="username"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>UserName</FormLabel>
+            <FormControl>
+              <Input placeholder="username" {...field} type="text" className="shad-input" />
+            </FormControl>
+           
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name="email"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Name</FormLabel>
+            <FormControl>
+              <Input placeholder="email" {...field} type="" className="shad-input" />
+            </FormControl>
+         
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name="password"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Password</FormLabel>
+            <FormControl>
+              <Input placeholder="password" {...field} type="password" className="shad-input" />
+            </FormControl>
+           
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <Button type="submit" className="shad-button_primary">
+        {
+          isLoading ? (<div className="flex-center gap-2">
+            <Loader/>
+          </div>):("Signup")
+        }
+        </Button>
+    </form>
+    </div>
+  </Form>
   )
 }
 
---
+++ src/components/shared/Loader.tsx
@@ -0,0 +1,19 @@
+
+
+const Loader = () => {
+  return (
+    <div className="flex-center w-full ">
+        <img
+        src="assets/icons/loader.svg"
+        alt=""
+        width={24}
+        height={24}
+        />
+
+
+
+    </div>
+  )
+}
+
+export default Loader
